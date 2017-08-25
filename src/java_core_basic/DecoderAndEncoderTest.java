38f00d1200e2335b23e89a2fedfcd2427638a250
tanglong <13685759344@163.com>
2017-08-25 18:09:18 +0800

    tanglong 2017/08/25 Java 字符串 编码和解码

---
+++ src/java_core_basic/DecoderAndEncoderTest.java
@@ -0,0 +1,27 @@
+package java_core_basic;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+public class DecoderAndEncoderTest {
+
+	public static void main(String[] args) {
+		String str = "今天真热啊!!!";
+		System.out.println("原始字符串:" + str);
+		// 编码
+		try {
+			String strEncode = URLEncoder.encode(str, "utf-8");
+			System.out.println("编码后的字符串:" + strEncode);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		// 解码
+		try {
+			String strDecode = URLDecoder.decode(str, "utf-8");
+			System.out.println("解码后的字符串:" + strDecode);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+}

7fbb395fd3e1e57b8a681548dc6b725535acfad6
Tanglong <13685759344@163.com>
2017-10-07 18:33:46 +0800

    update

--- src/java_web_programming/LocalHostTest.java
+++ src/java_web_programming/LocalHostTest.java
@@ -4,13 +4,12 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class LocalHostTest {
-	@SuppressWarnings("static-access")
 	public static void main(String[] args) throws UnknownHostException {
 		InetAddress host = InetAddress.getLocalHost();
-		String ip =host.getHostAddress();
-		String name = host.getHostName();
-		System.out.println("本机名称：" + name);
-		System.out.println("本机Ip地址：" + ip);
-		System.out.println("本机固定Ip："+host.getLoopbackAddress());
+		System.out.println("本机名称：" + host.getHostName());
+		System.out.println("本机Ip地址：" + host.getHostAddress());
+		System.out.println("本机回路Ip："+InetAddress.getLoopbackAddress());
+		System.out.println("本机回路Ip："+InetAddress.getByName(null));
+		System.out.println("本机回路Ip："+InetAddress.getByName("127.0.0.1"));
 	}
 }

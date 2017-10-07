363b84d7908c438158cf4b5a9bc24003bcbf92f5
Tanglong <13685759344@163.com>
2017-10-07 13:14:57 +0800

    compareTo

--- src/java_core_basic/ArrayTest.java
+++ src/java_core_basic/ArrayTest.java
@@ -11,22 +11,31 @@ public class ArrayTest {
 		//数据初始化
 		int[] intArr = new int[10];
 		for (int i = 0; i < intArr.length; i++) {
-			intArr[i] = (int) (Math.random() * 100);
+			intArr[i] = (int) (Math.random() * 10);
 		}
-		System.out.print("Before Sort:");
+		//Before sort
+		System.out.print("Before Sort:\n");
 		for (int var : intArr) {
 			System.out.print(var + "\t");
 		}
 		System.out.println();
-		//排序
+		//Sort
 		Arrays.sort(intArr);
 
-		System.out.print("After Sort:");
-		System.out.println();
+		//After sort
+		System.out.print("After Sort:\n");
 		for (int var : intArr) {
 			System.out.print(var + "\t");
 		}
 		System.out.println();
 		System.out.println("数组.toString()" + intArr.toString());
+
+		//Binary search
+		//param:数组
+		//param:检索开始位置
+		//param:检索结束位置
+		//param:查找数值
+		//找到返回数值所在的位置，未找到返回负数
+		System.out.println(Arrays.binarySearch(intArr, 0,intArr.length,5));
 	}
 }
--- src/java_core_basic/StringTest.java
+++ src/java_core_basic/StringTest.java
@@ -4,19 +4,20 @@ public class StringTest {
 	public static void main(String[] args) {
 		String str1 = "the light";
 		String str2 = new String(str1);
-
 		// '=='用于判断是否是同一个字符串对象
-		System.out.print("str1 == str2:");
-		System.out.println(str1 == str2);
+		System.out.println("str1 == str2:" + str1 == str2);
+
 		String str11 = "the light";
 		String str22 = "the light";
-		System.out.print("str11 == str22:");
-		System.out.println(str11 == str22);
+		System.out.println("str11 == str22:" + str11 == str22);
+
 		String str111 = "The Light";
 		String str222 = "the light";
 		//equals 用于判断字符串的值
 		System.out.println("str111.equals(str222):" + str111.equals(str222));
 		System.out.println("str111.equalsIgnoreCase(str222):" + str111.equalsIgnoreCase(str222));// 忽略大小写进行比较
+		System.out.println("str111.str111.compareTo(str222):" + str111.compareTo(str222));
+		System.out.println("str111.compareToIgnoreCase(str222):" + str111.compareToIgnoreCase(str222));
 		System.out.println("str111.startsWith(\"the\"):" + str111.startsWith("the"));
 		System.out.println("str111.endsWith(\"ght\"):" + str111.endsWith("ght"));
 

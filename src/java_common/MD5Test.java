712c7d9728e75462463cee24bec4b647e7a30479
Tanglong <13685759344@163.com>
2017-10-19 18:04:20 +0800

    update

--- src/java_common/MD5Test.java
+++ src/java_common/MD5Test.java
@@ -1,3 +1,9 @@
+/**
+ * digest()方法返回字符串s的md5哈希值，是16位 的十六进制（相当于8个字符）
+ * BigInteger将8个字符构成的字符串转换成16位的十六进制， 并用字符串来表示，
+ * 从而得到字符串形式的哈希值
+ */
+
 package java_common;
 
 import java.math.BigInteger;
@@ -5,11 +11,15 @@ import java.security.MessageDigest;
 
 public class MD5Test {
 	/**
-	 * digest()方法返回字符串s的md5哈希值，是16位 的十六进制（相当于8个字符）
-	 * BigInteger将8个字符构成的字符串转换成16位的十六进制， 并用字符串来表示，
-	 * 从而得到字符串形式的哈希值
+	 * MD5 加密
+	 * @param s 待加密字符串
+	 * @return 加密后的字符串
 	 */
 	public static String makeMD5(String s) {
+		// 空值判断
+		if(s.equals("") || null == s){
+			return "";
+		}
 		try {
 			// 生成一个MD5加密计算摘要
 			MessageDigest md = MessageDigest.getInstance("MD5");

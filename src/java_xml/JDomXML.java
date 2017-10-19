8b63dc95edf25a56eb9ea1bb7102ddfa8b25eca9
Tanglong <13685759344@163.com>
2017-10-19 16:34:50 +0800

    JDom Parse XML

--- src/java_xml/JDomXML.java
+++ src/java_xml/JDomXML.java
@@ -9,29 +9,58 @@ package java_xml;
 import java.io.FileInputStream;
 import java.util.List;
 
+import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.input.SAXBuilder;
 
 public class JDomXML {
 	private String fileName = "XML/poem.xml";
+	Document document = null;
 
 	public JDomXML(){
 		try{
 			SAXBuilder saxBuilder = new SAXBuilder();
-			Document document = saxBuilder.build(new FileInputStream(fileName));
-			//获取根节点bookstore
-			Element rootElement = document.getRootElement();
-			System.out.println("rootElement:"+rootElement);
-			//获取根节点的子节点，返回子节点的数组
-			List<Element> poemList = rootElement.getChildren();
-			poemList.forEach(s->System.out.println("s="+s));
+			document = saxBuilder.build(new FileInputStream(fileName));
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * print XML info
+	 */
+	public void printMsg(){
+		Element rootElement = document.getRootElement();
+		System.out.println(rootElement);
+		// get children nodes as list
+		List<Element> poemList = rootElement.getChildren();
+		// print children elements info
+		poemList.forEach(s->printElement(s));
+	}
+
+	/**
+	 * print element info
+	 * @param element
+	 */
+	public void printElement(Element e){
+		// element name
+		System.out.print(e.getName() + "\t");
+		// print attributes
+		List<Attribute> attributes = e.getAttributes();
+		attributes.forEach(s->System.out.print(s.getName() + ":" + s.getValue() + "\t"));
+		System.out.println();
+		// print text that is empty
+		if(!(e.getTextTrim().equals(""))){
+			System.out.print(e.getTextTrim());
+		}
+		// loop for children nodes
+		List<Element> poemList = e.getChildren();
+		poemList.forEach(s->printElement(s));
+	}
+
+	// This is a test
 	public static void main(String[] args) {
-		new JDomXML();
+		new JDomXML().printMsg();
 	}
 }

0bfaed59826b9550c85cafdbd5dc4f74a115d49b
Tanglong <13685759344@163.com>
2017-10-23 21:37:22 +0800

    检验if循环语句的执行顺序

---
+++ src/java_core_basic/IfExecuteOrderTest.java
@@ -0,0 +1,22 @@
+/**
+ * 检验if的执行顺序
+ */
+
+package java_core_basic;
+
+public class IfExecuteOrderTest {
+	public static void main(String[] args) {
+		int i=0;
+		//测试
+		//预期结果：ABDCBDCB
+		for(test('A');test('B')&&i<2;i++,test('C')){
+			test('D');
+		}
+	}
+
+	//测试函数
+	static boolean test(char ch){
+		System.out.print(ch);
+		return true;
+	}
+}

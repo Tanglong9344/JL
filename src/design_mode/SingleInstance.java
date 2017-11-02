ce8b32e25bfeadd895aa37c070be07a63a5705af
Tanglong <13685759344@163.com>
2017-11-02 23:00:00 +0800

    Update

--- src/design_mode/SingleInstance.java
+++ src/design_mode/SingleInstance.java
@@ -1,5 +1,8 @@
 /**
- * 这个类有且只有一个实例(单例模式)
+ * 单例模式
+ * 一个类仅有一个实例
+ * 并提供一个访问它的全局访问点
+ *
  * 参看RunTime类
  */
 
@@ -7,7 +10,7 @@ package design_mode;
 
 public class SingleInstance {
 	//静态常量实例对象
-	static final SingleInstance singleInstance=new SingleInstance();
+	private static final SingleInstance singleInstance=new SingleInstance();
 	//返回实例对象
 	public SingleInstance getSingleInstance(){
 		return singleInstance;

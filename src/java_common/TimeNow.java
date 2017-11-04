c7b6aaed87d8e0b3acc25af18d77324a89364bef
Tanglong <13685759344@163.com>
2017-11-04 11:06:48 +0800

    get time(yyyy-MM-dd HH:mm:ss)

---
+++ src/java_common/TimeNow.java
@@ -0,0 +1,17 @@
+/**
+ * 获取系统当前时间
+ * 以yyyy-MM-dd HH:mm:ss格式返回
+ */
+
+package java_common;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeNow {
+	public static String getTimeNow(){
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date date = new Date();
+		return sdf.format(date);
+	}
+}

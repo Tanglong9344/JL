85de1ad72b0a206bd5a3cd013d1782d22da8b107
Tanglong <13685759344@163.com>
2017-11-06 18:23:08 +0800

    Thread Priority

---
+++ src/java_thread/ThreadPriority.java
@@ -0,0 +1,39 @@
+package java_thread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreadPriority {
+	//This is a test
+	public static void main(String[] args) {
+		final int N=10;
+		List<ThreadP> tList = new ArrayList<ThreadP>();
+		for(int i=1;i<=N;i++){
+			ThreadP tp = new ThreadP("Thread"+i);
+			//设置优先级
+			tp.setPriority((new java.util.Random().nextInt(N)+1));
+			tList.add(tp);
+		}
+		//启动
+		tList.forEach(tp->tp.start());
+	}
+}
+
+/**线程类*/
+class ThreadP extends Thread{
+	public ThreadP(String name){
+		super(name);
+	}
+	@Override
+	public void run(){
+		int a=6;
+		while((a--)>0){
+			System.out.println("线程名："+this.getName()+" 优先级："+this.getPriority());
+			try {
+				Thread.sleep(1000);//休眠1s
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}

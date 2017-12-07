1b7d5f675e2c32672aca19589aa19987e6f007a4
Tanglong <13685759344@163.com>
2017-12-07 20:02:00 +0800

    Update

--- src/java_other/SHATest.java
+++ src/java_other/SHATest.java
@@ -19,21 +19,16 @@ public class SHATest {
 		System.out.println("SHA-1:" + encryptSHA(key,false));
 	}
 
-	public static String encryptSHA(String s,boolean choose) throws Exception {
+	public static String encryptSHA(String s,boolean cho) throws Exception {
 		// 空值判断
 		if(s.equals("") || null == s){
 			return "";
 		}
 		//创建具有指定算法名称的信息摘要
-		MessageDigest sha = null;
-		if(choose){
-			sha = MessageDigest.getInstance("SHA");
-		}else{
-			sha = MessageDigest.getInstance("SHA-1");
-		}
+		MessageDigest md = cho?MessageDigest.getInstance("SHA"):MessageDigest.getInstance("SHA-1");
 		//使用指定的字节数组对摘要进行最后更新
-		sha.update(s.getBytes());
+		md.update(s.getBytes());
 		// 完成摘要计算
-		return (new BigInteger(1,sha.digest()).toString(16));
+		return (new BigInteger(1,md.digest()).toString(16));
 	}
 }

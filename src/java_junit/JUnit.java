6cef5873fffc8757ec364b9a0029c910232d637d
Tanglong <13685759344@163.com>
2017-12-18 16:05:21 +0800

    JUnit

---
+++ src/java_junit/JUnit.java
@@ -0,0 +1,18 @@
+package java_junit;
+
+/**
+ * JUnit4通过注解的方式来识别测试方法。
+ * 目前支持的主要注解有：
+ * 1. @BeforeClass 全局只会执行一次，而且是第一个运行
+ * 2. @Before 在测试方法运行之前运行
+ * 3. @Test 测试方法
+ * 4. @After 在测试方法运行之后允许
+ * 5. @AfterClass 全局只会执行一次，而且是最后一个运行
+ * 6. @Ignore 忽略此方法
+ *
+ * @author 唐龙
+ *
+ */
+public class JUnit {
+
+}

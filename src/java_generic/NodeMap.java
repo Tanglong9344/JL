9a5ff2ee9b20b96ce686c4aca7be85a36b8ccc1b
Tanglong <13685759344@163.com>
2017-12-23 10:59:12 +0800

    Update

--- src/java_generic/NodeMap.java
+++ src/java_generic/NodeMap.java
@@ -12,48 +12,45 @@ public class NodeMap {
 	public static void main(String[] args) {
 		// Map Function
 		Function<Integer, Integer> f = (Integer x)->(2*x);
-		// Node
-		Node<Integer> head = new Node<Integer>(1);
-		Node<Integer> node1 = new Node<Integer>(2);
-		head.next = node1;
-		Node<Integer> node2 = new Node<Integer>(3);
-		node1.next = node2;
-
-		System.out.println("------Before Changed------");
+		// Initialize Node
+		Node<Integer> head = new Node<Integer>(1);//Head Node
+		Node<Integer> node = head;
+		for(int i=2;i<=10;i++){
+			node.next = new Node<Integer>(i);
+			node = node.next;
+		}
+		System.out.println("\n------Before Changed------");
 		view(head);
 
+		// Map Node
+		System.out.println("\n------After Changed------");
 		Node<Integer> h = map(head,f);
-		System.out.println("------After Changed------");
 		view(h);
 	}
 
 	/**Node Map*/
+	@SuppressWarnings("unchecked")
 	static <T, R> Node<R> map(Node<T> head, Function<T, R> f) {
 		if(head == null){
 			return null;
 		}
-		@SuppressWarnings("unchecked")
-		Node<R> data = new Node<R>(f.apply((T) head.data));
-		Node<R> h =data;
-		while(head.next != null){
-			head = head.next;
-			@SuppressWarnings("unchecked")
-			Node<R> data2 = new Node<R>(f.apply((T) head.data));
-			data.next = data2;
-			data=data2;
+		Node<R> node = new Node<R>(f.apply((T) head.data));
+		Node<R> h = node;
+		while((head=head.next) != null){
+			node.next = new Node<R>(f.apply((T) head.data));
+			node = node.next;
 		}
 		return h;
 	}
 
-	/**Travers Nodes*/
+	/**Traverse Nodes*/
 	static void view(Node<?> node){
-		if(node == null){
-			return;
-		}
-		System.out.println(node.data);
-		while(node.next != null){
+		while(node != null){
+			System.out.print(node.data);
 			node = node.next;
-			System.out.println(node.data);
+			if(node != null) {
+				System.out.print("->");
+			}
 		}
 	}
 }

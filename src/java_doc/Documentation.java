24d306a5ed3111a6a2cfe8e4cffe70596cc438e8
Administrator <Administrator@3RBM3KKZT40ZEN9>
2017-12-25 17:52:33 +0800

    Documentation

---
+++ src/java_doc/Documentation.java
@@ -0,0 +1,11 @@
+package java_doc;
+
+/**
+ * The Java API Documentation
+ *
+ * {@link https://docs.oracle.com/javase/7/docs/technotes/tools/windows/javadoc.html}
+ *
+ * @author ÌÆÁú
+ *
+ */
+public class Documentation {}

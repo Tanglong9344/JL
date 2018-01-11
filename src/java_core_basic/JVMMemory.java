fdb81c42cff7233497bc9da44c4c6377d643ddf9
Tanglong <13685759344@163.com>
2018-01-11 22:31:18 +0800

    JVM Memory

---
+++ src/java_core_basic/JVMMemory.java
@@ -0,0 +1,31 @@
+package java_core_basic;
+
+/**
+ * The memory of JVM
+ *
+ * @author ÌÆÁú
+ *
+ */
+public class JVMMemory {
+	public static void main(String[] args) {
+		final int M = 1024*1024;
+		long maxMemory       = Runtime.getRuntime().maxMemory()/M;
+		long totalUsedMemory = Runtime.getRuntime().totalMemory()/M;
+		System.out.println("MaxMemory      : " + maxMemory + "M");
+		System.out.println("TotalUsedMemory: " + totalUsedMemory + "M");
+
+		String s = "";
+		for(int i=0;i<10000;i++){
+			s += i;
+		}
+		System.out.println(s);
+		totalUsedMemory = Runtime.getRuntime().totalMemory()/M;
+		System.out.println("TotalUsedMemory: " + totalUsedMemory + "M");
+
+		for(int i=0;i<10000_0000;i++){
+			new String("" + i);
+		}
+		totalUsedMemory = Runtime.getRuntime().totalMemory()/M;
+		System.out.println("TotalUsedMemory: " + totalUsedMemory + "M");
+	}
+}

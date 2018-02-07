245b668c7e6bc5edba94c54619e4f317f090049c
Tanglong <13685759344@163.com>
2018-02-07 17:46:51 +0800

    Non Blocking IO

--- src/java_nio/NIO.java
+++ src/java_nio/NIO.java
@@ -1,9 +1,17 @@
 package java_nio;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Java NIO
  *
  * @author ÌÆÁú
  *
  */
-public class NIO {}
+public class NIO {
+	public static void main(String[] args) {
+		Path resource = Paths.get("file");
+		System.out.println(resource);
+	}
+}

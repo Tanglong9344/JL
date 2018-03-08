c6ba22ec0d0cf1ba880fcb3b7c4246d8582b2b55
Tanglong <13685759344@163.com>
2018-03-08 12:04:02 +0800

    Void class

---
+++ src/java_details/VoidClassTest.java
@@ -0,0 +1,19 @@
+package java_details;
+
+/**
+ * java.lang.Void
+ *
+ * @author tanglong
+ *
+ */
+public class VoidClassTest {
+	public static void main(String[] args) {
+		voidTest();
+	}
+
+	/** Void class */
+	private static Void voidTest() {
+		System.out.println("Void Class Test...");
+		return null;
+	}
+}

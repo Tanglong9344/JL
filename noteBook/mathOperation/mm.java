5ed78bb06e94bbd9456d3a4cd4a9ba4c72e1810b
Tanglong9344 <13685759344@163.com>
2019-01-09 17:04:43 +0800

    Create mm.java

---
+++ noteBook/mathOperation/mm.java
@@ -0,0 +1,4 @@
+System.out.println(Integer.MAX_VALUE);//2147483647
+System.out.println(Integer.MIN_VALUE);//-2147483648
+System.out.println(Integer.MAX_VALUE  + 1);//-2147483648
+System.out.println(Integer.MIN_VALUE-1);//2147483647

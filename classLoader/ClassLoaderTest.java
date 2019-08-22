6e96421675e1969f7ca2894ef1939861f12d15a0
Tanglong9344 <13685759344@163.com>
2019-08-22 20:07:41 +0800

    Update ClassLoaderTest.java

--- classLoader/ClassLoaderTest.java
+++ classLoader/ClassLoaderTest.java
@@ -1,9 +1,17 @@
+import java.net.URL;
+
 public class ClassLoaderTest {
     public static void main(String[] args ) {
        ClassLoader cl = ClassLoaderTest.class.getClassLoader();
        while (cl!=null){
            System.out.println(cl.getClass());
            cl=cl.getParent();
+       }
+       
+       // 查看BootStrap已加载的所有类库
+       URL[] urLs = sun.misc.Launcher.getBootstrapClassPath().getURLs();
+       for (java.net.URL url : urLs) {
+           System.out.println(url.toExternalForm()) ;
        }
     }
 }
